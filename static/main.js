667f40a5706707091cb33947bf5e051deaff1264
Lucas Manning <lucas.manning21@gmail.com>
2022-11-30 22:18:16 -0800

    make sure scroll bars don't appear

--- static/main.js
+++ static/main.js
@@ -22,14 +22,14 @@ function step(timestamp) {
     const timestep = timestamp - previousTimeStamp;
 
     if (timestep > 0) {
-        x = Math.max(-rect.left, Math.min(x + dx*timestep, window.innerWidth - rect.right));
-        y = Math.max(-rect.top, Math.min(y + dy*timestep, window.innerHeight - rect.bottom));
+        x = Math.max(-rect.left, Math.min(x + dx*timestep, window.innerWidth - rect.right - 1));
+        y = Math.max(-rect.top, Math.min(y + dy*timestep, window.innerHeight - rect.bottom - 1));
         titleElem.style.transform = `translate(${x}px, ${y}px)`;
-        if (x === -rect.left || x + rect.right === window.innerWidth) {
+        if (x === -rect.left || x + rect.right === window.innerWidth - 1) {
             titleElem.style.color = randomColor()
             dx = -dx;
         }
-        if (y === -rect.top || y + rect.bottom === window.innerHeight) {
+        if (y === -rect.top || y + rect.bottom === window.innerHeight - 1) {
             titleElem.style.color = randomColor()
             dy = -dy;
         }
