689094e3bfb6fcd9d1b8081cb23dd28a39952f35
Lucas Manning <lucas.manning21@gmail.com>
2022-12-02 18:17:40 -0800

    clean up js and attempt to fix safari bug

--- static/main.js
+++ static/main.js
@@ -1,15 +1,10 @@
 let titleElem = document.getElementById("title");
 let rect = titleElem.getBoundingClientRect();
-let previousTimeStamp = 0;
 let x = 0;
 let y = 0;
 let dx = 0.3;
 let dy = 0.3;
-
-function randomColor() {
-    let deg = Math.random() * 360
-    return `hue-rotate(${deg}deg)`; 
-}
+let previousTimeStamp = 0;
 
 function step(timestamp) {
     const timestep = timestamp - previousTimeStamp;
@@ -17,29 +12,52 @@ function step(timestamp) {
     if (timestep > 0) {
         x = Math.max(-rect.left, Math.min(x + dx*timestep, window.innerWidth - rect.right - 1));
         y = Math.max(-rect.top, Math.min(y + dy*timestep, window.innerHeight - rect.bottom - 1));
-        titleElem.style.transform = `translate(${x}px, ${y}px)`;
+        setTransform("");
         if (x === -rect.left || x + rect.right === window.innerWidth - 1) {
-            titleElem.style.filter = randomColor() 
+            setFilter(randomColor());
             dx = -dx;
         }
         if (y === -rect.top || y + rect.bottom === window.innerHeight - 1) {
-            titleElem.style.filter = randomColor() 
+            setFilter(randomColor());
             dy = -dy;
         }
+        setTransform(`translate(${x}px, ${y}px)`);
     }
 
     previousTimeStamp = timestamp;
     window.requestAnimationFrame(step);
 }
 
-let id = window.requestAnimationFrame(step);
-
-window.addEventListener("resize", (e) => {
+function reset() {
     window.cancelAnimationFrame(id);
-    titleElem.style.transform = `translate(0px, 0px)`;
-    titleElem.style.filter = `grayscale(100%) brightness(1000%)`;
     x = 0;
     y = 0;
+    dx = 0.3;
+    dy = 0.3;
+    setFilter("grayscale(100%) brightness(1000%)");
+    setTransform("translate(0px, 0px)");
     rect = titleElem.getBoundingClientRect();
     id = window.requestAnimationFrame(step);
+}
+
+function randomColor() {
+    let deg = Math.random() * 360
+    return `hue-rotate(${deg}deg)`; 
+}
+
+function setTransform(val) {
+    titleElem.style.transform = val;
+    titleElem.style.webkitTransform = val;
+}
+
+function setFilter(val) {
+    titleElem.style.filter = val;
+    titleElem.style.webkitFilter = val;
+}
+
+
+let id = window.requestAnimationFrame(step);
+window.addEventListener("resize", (e) => {
+    reset();
 })
+
